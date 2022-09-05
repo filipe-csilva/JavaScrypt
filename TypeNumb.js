ba538c369a2eeb2225769833e6c72a4fba5fa14f
unknown <filipepaulocs@gmail.com>
2022-09-04 23:31:50 -0300

    exemplos da aula 1

---
+++ TypeNumb.js
@@ -0,0 +1,23 @@
+const meuNumero = 3;
+
+const primeiroNumero = 5;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFluuante = 3.3;
+const pontoFlutuanteSemOZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFluuante;
+
+console.log(novaOperacao);
+
+//NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
---
+++ TypeString.js
@@ -0,0 +1,19 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+
+const senha = "SenhaSuperSegura456!";
+const stringDeNumber = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+console.log(citacao);
+
+//Conscatenização
+
+console.log(texto1 + " " + senha)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
+
