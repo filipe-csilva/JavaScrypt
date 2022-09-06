4396d0eebd72551d5f6e98985aa5f12edba00f63
unknown <filipepaulocs@gmail.com>
2022-09-05 22:50:53 -0300

    exemplos da aula 1

---
+++ templateString.js
@@ -0,0 +1,12 @@
+const nome = "JU";
+const idade = 2021-1981;
+const cidadedenascimento = "São Paulo";
+
+const apresentacao1 = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadedenascimento;
+
+
+//acento clase
+const apresentacao2 = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadedenascimento}`;
+
+console.log(apresentacao1);
+console.log(apresentacao2);
