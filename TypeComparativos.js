3dbac87fa91eae5e9189f6530981f5e8d9833f0e
unknown <filipepaulocs@gmail.com>
2022-09-05 22:20:51 -0300

    exemplos da aula 1

---
+++ TypeComparativos.js
@@ -0,0 +1,11 @@
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto);
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(numero === Number(texto));
+console.log(String(numero) === texto);
+
