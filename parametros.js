9a96ce8b4fcc331579c50822ff1a501cc10ab4eb
unknown <filipepaulocs@gmail.com>
2022-09-06 20:41:46 -0300

    exemplos da aula 1

---
+++ parametros.js
@@ -0,0 +1,9 @@
+function soma(num1,num2){
+    return num1 + num2;
+}
+
+console.log(soma(0005000,2));
+console.log(soma(500,20));
+console.log(soma(2,204400));
+console.log(soma(02,2000));
+console.log(soma(20000,2));
