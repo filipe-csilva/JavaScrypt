399952918b9b8843406d627cae17c7c2ef8cc612
filipeleasing <filipepaulocs@gmail.com>
2022-10-16 09:55:04 -0300

    juntando turmas

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo'];
+const salaDeJavaScript = ['Ju', 'Leo', 'Rquel'];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript);
+
+console.log (salasUnificadas);
