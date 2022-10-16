4fc05d10a848c20e1d55ec8d9465f80ef331e86e
filipeleasing <filipepaulocs@gmail.com>
2022-10-16 13:00:26 -0300

    adicionando

--- localizando-aluno.js
+++ localizando-aluno.js
@@ -6,7 +6,7 @@ let listaDeNotasEAlunos = [alunos,mediasDosAlunos];
 
 const exibeNomeNota = (nomeDoAluno) => {
     if(listaDeNotasEAlunos[0].includes(nomeDoAluno)){
-        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno);
+        let indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno);
         return listaDeNotasEAlunos[0][indice] + ', sua media é ' + listaDeNotasEAlunos[1][indice]
     }else{
         return "Aluno não está cadastrado"
