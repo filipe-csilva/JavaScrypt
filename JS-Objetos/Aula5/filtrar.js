a045f38b376136576e14c09f7fe891d490157e44
filipeleasing <filipepaulocs@gmail.com>
2022-12-25 18:03:40 -0300

    1.0.0

---
+++ JS-Objetos/Aula5/filtrar.js
@@ -0,0 +1,11 @@
+const clientes = require("./clientes.json");
+
+function filtarApartamentosSemComplementos(clientes){
+    return clientes.filter((cliente) => {
+        return (cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty("complemento"))
+    });
+}
+
+const filtrados = filtarApartamentosSemComplementos(clientes);
+
+console.log(filtrados);
